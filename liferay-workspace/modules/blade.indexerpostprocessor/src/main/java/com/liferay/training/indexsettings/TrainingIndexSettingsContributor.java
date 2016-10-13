e687b6e642407e73b21aec169a9ba95cb3780e02
Tibor Lipusz <tibor.lipusz@liferay.com>
2016-10-13 17:12:34 +0200

    LRTRAINING-X IndexerPostProcessor Demo - Add IndexSettingsContributor to add custom mapping for the new field as "additionalTypeMappings" for Elasticsearch (in progress)

---
+++ liferay-workspace/modules/blade.indexerpostprocessor/src/main/java/com/liferay/training/indexsettings/TrainingIndexSettingsContributor.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.training.indexsettings;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.search.elasticsearch.settings.BaseIndexSettingsContributor;
+import com.liferay.portal.search.elasticsearch.settings.IndexSettingsContributor;
+import com.liferay.portal.search.elasticsearch.settings.TypeMappingsHelper;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Tibor Lipusz
+ */
+@Component(
+	configurationPid = "com.liferay.portal.search.elasticsearch.configuration.ElasticsearchConfiguration",
+	immediate = true, service = IndexSettingsContributor.class
+)
+public class TrainingIndexSettingsContributor
+	extends BaseIndexSettingsContributor {
+
+	public TrainingIndexSettingsContributor() {
+		super(1);
+	}
+
+	public TrainingIndexSettingsContributor(int priority) {
+		super(priority);
+	}
+
+	@Override
+	public void contribute(TypeMappingsHelper typeMappingsHelper) {
+		long companyId = CompanyThreadLocal.getCompanyId();
+
+		String trainingTypeMappings = getResourceAsString(
+			getClass(), _TRAINING_LIFERAY_TYPE_MAPPING_FILE_NAME);
+
+		_log.info("Contributing to type mapping for company " + companyId);
+
+//		typeMappingsHelper.addTypeMappings(
+//			getIndexName(companyId), trainingTypeMappings);
+	}
+
+	/**
+	 * See {@link
+	 *  com.liferay.portal.search.elasticsearch.internal.util.ResourceUtil.
+	 *   getResourceAsString(Class<?>, String)}
+	 *
+	 * @param clazz
+	 * @param resourceName
+	 * @return
+	 */
+	protected String getResourceAsString(Class<?> clazz, String resourceName) {
+		try (InputStream inputStream = clazz.getResourceAsStream(
+				resourceName)) {
+
+			return StringUtil.read(inputStream);
+		}
+		catch (IOException ioe) {
+			throw new RuntimeException(
+				"Unable to load resource: " + resourceName, ioe);
+		}
+	}
+
+	private static final String _TRAINING_LIFERAY_TYPE_MAPPING_FILE_NAME =
+		"/META-INF/mappings/training-liferay-type-mappings.json";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		TrainingIndexSettingsContributor.class);
+
+}
